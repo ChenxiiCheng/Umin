61bc73b320ac1fa98bad9b7a6f0155ad0b8bd931
ChenxiiCheng <chenxic1011@gmail.com>
2020-11-01 13:31:25 -0500

    build: set up development environment and add framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,16 @@ import {
   ChakraProvider,
   CSSReset,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
 } from "@chakra-ui/core"
 import theme from "@chakra-ui/theme"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <CSSReset />
     <Box textAlign="center" fontSize="xl">
-      <Grid
+      hello world
+      {/* <Grid
         minH="100vh"
         p={3}
         direction="column"
@@ -25,22 +20,7 @@ export const App = () => (
         justify="center"
       >
         <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
+      </Grid> */}
     </Box>
   </ChakraProvider>
 )
